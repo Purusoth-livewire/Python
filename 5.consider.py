09edc6d9f27fcbfbf47894c856f9b5000cb523cf
brooze <56531358+Purusoth-livewire@users.noreply.github.com>
2021-04-20 14:08:08 +0530

    Add files via upload

---
+++ 5.consider.py
@@ -0,0 +1,77 @@
+consider to python:
+===================
+1.identifier:
+2.indentation
+3.keywords
+4.comments
+
+
+1.indentifier:
+==============
+variable,function name, class name,object name
+
+user defined text.
+
+rules:
+======
+1.identifier must be contain with alphabet,numbers and underscore(_).
+2.identifier are case sentivity.(upper and lower are not equal)
+3.do not start with number.
+4.space b/w identifier are not allowed.
+5.symbols are not allowed.
+6.keywords are not used as a identier.
+7.if declare a function name starting letter should be lowercase.(show,getData,studentDetails)
+8.if declare a class name starting  letter should be uppercase.(Show,GetData,StudentDetails)
+
+
+2.indentation:
+==============
+allocate 4 space or tab
+
+if(condition)#other language
+{
+    statements
+}
+
+
+#in python
+if condition:
+    statements#tab---block
+    statements
+else:
+    statements
+    statements
+
+3.Keyword:
+==========
+reserved word for the program
+
+ex:
+for,if,True,False,while,pass
+
+
+4.comments:
+===========
+1.single line comment
+2.multi line comment
+
+1.single line comment(#):
+=========================
+hide the details for that line.
+
+#details
+
+2.Multiline comment:
+====================
+
+"""multiline comment"""
+'''multiline comment'''
+
+
+a=10
+#a=a+10
+print(a)
+
+
+b=20
+print(b)
---
+++ 6.datatype.py
@@ -0,0 +1,24 @@
+"""
+Data Type:
+==========
+1.number
+2.string
+3.list
+4.set
+5.tuple
+6.dictionary
+
+1.number:
+=========
+integer#1,2,3,100,5000 no range
+float#no range but accuracy 16 digit
+complex
+"""
+
+#type(variable or data)
+
+a=1.12345678901234567890
+print(a)
+print("%.2f"%(a))
+      
+

d9b27c28b5e263ee6d6952038704cf339bfbc612
brooze <56531358+Purusoth-livewire@users.noreply.github.com>
2021-04-21 14:34:22 +0530

    Add files via upload

---
+++ 6.datatype.py
@@ -0,0 +1,72 @@
+Data Type:
+==========
+1.number
+2.string
+3.list
+4.set
+5.tuple
+6.dictionary
+7.Boolean
+
+1.number:
+=========
+integer#1,2,3,100,5000 no range
+float #no range but accuracy 16 digit
+complex
+
+
+#type(variable or data)
+val=10
+print(val)
+print(type(val))
+
+a=1.12345678901234567890
+print(a)
+print("%.2f"%(a))
+print(type(a))
+      
+com=1+2j
+print(com)
+print(type(com))
+
+
+
+2.Boolean:
+=========
+bolean will return true or false.
+
+
+a=10
+b=10
+
+c=a==b
+print(c)
+
+c=a!=b
+print(c)
+
+3.String:
+========
+to store names
+
+n1='sabarivasan'
+print(n1)
+
+n2="Karthick"
+print(n2)
+
+n3="kishore can'tcame pyhton class"
+print(n3)
+
+
+n4=""" "kishore ", can't came pyhton class """
+print(n4)
+
+n5=''' "kishore ", can't came pyhton class '''
+print(n5)
+
+
+'''hide'''
+"""hide"""
+
+

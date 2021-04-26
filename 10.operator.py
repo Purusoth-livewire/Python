4452af53e205cbe1ae0fad1a56136f5c3de04938
brooze <56531358+Purusoth-livewire@users.noreply.github.com>
2021-04-26 14:48:16 +0530

    Add files via upload

---
+++ 10.operator.py
@@ -0,0 +1,61 @@
+"""
+Operator:
+=========
+1.arithmatic Operator
+2.Relational Operator
+3.Logical Operator
+4.Bitwise Operator
+5.Assignent Operator
+6.Special OPerator
+
+
+1.arithmatic Operator:
+=====================
+(+)   ----> Addition
+(-)   ----> Subtraction
+(*)   ----> Multiplication
+(/)   ----> Division
+(//) ----> Floor Division
+(%)  ----> Modulus
+(**)  ----> Exponential
+
+
+
+a=27
+b=8
+c=a+b
+print("add : ",c)
+c=a-b
+print("sub : ",c)
+c=a*b
+print("mul : ",c)
+c=a/b
+print("div : ",c)
+c=a//b
+print("f.div : ",c)
+c=a%b
+print("mod : ",c)
+c=a**b
+print("exp  : ",c)
+
+"""
+
+a=10
+b=2
+#expression
+
+add=a+b   #c=12
+sub=a-b   #c=8
+mul=a*b   #c=20
+div1=a/b   #c=5.0
+div2=a//b   #c=5
+mod=a%b   #c=0
+exp=a**b   #c=100
+#result
+print("add : ",add)
+print("sub : ",sub)
+print("mul : ",mul)
+print("div : ",div1)
+print("f.div : ",div2)
+print("mod : ",mod)
+print("exp  : ",exp)
